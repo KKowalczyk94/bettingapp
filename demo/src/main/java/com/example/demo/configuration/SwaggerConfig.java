5c5017db706074573af6c2504f913db4eb032e7c
unknown <karolxkow@gmail.com>
2024-06-27 11:16:10 +0000

    Trying to add swagger

---
+++ demo/src/main/java/com/example/demo/configuration/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package com.example.demo.configuration;//package com.example.demo.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public  Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.demo.controllers"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
+
+//    @Bean
+//    public OpenAPI customOpenAPI() {
+//        return new OpenAPI()
+//                .info(new Info()
+//                        .title("Demo API")
+//                        .version("1.0")
+//                        .description("API documentation for the Demo application"));
+//    }
+//
+//    @Bean
+//    public GroupedOpenApi publicApi() {
+//        return GroupedOpenApi.builder()
+//                .group("public")
+//                .pathsToMatch("/**")
+//                .build();
+//    }
+
--- demo/src/main/java/com/example/demo/controllers/HomeController.java
+++ demo/src/main/java/com/example/demo/controllers/HomeController.java
@@ -1,18 +1,24 @@
 package com.example.demo.controllers;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("home")
+@RequestMapping("/")
+@Api(tags = "Home Controller", description = "Home controller operations")
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping
+    @ApiOperation(value = "Get Home", notes = "Returns a welcome message")
     public String home() {
         return "Welcome to the Home Page!";
     }
 
     @GetMapping("/hello")
+    @ApiOperation(value = "Say Hello", notes = "Returns a hello message")
     public String hello() {
         return "Hello, World!";
     }
